8d0f5a7c6ad9c75565540e01dcd0ffc216c43005
Nicoleta Pavelescu <nicoletap@uia.no>
2024-04-26 22:43:19 +0200

    Stringd

--- script.js
+++ script.js
@@ -31,6 +31,41 @@ const restaurant = {
     },
   },
 };
+// //Strings -------------------------------------
+// const airline = 'TAP Air Portugal';
+// const plane = 'A320';
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log('B737'[0]);
+
+// console.log(airline.length);
+// console.log('B737'.length);
+// console.log(airline.lastIndexOf('r'));
+// console.log(airline.indexOf('portugal')); //case sensitive
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
+
+// const checkMiddleSeat = function (seat) {
+//   //B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') console.log('You got the middle seat');
+//   else console.log('You got lucky');
+// };
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('11A');
+
+// console.log(new String('jonas'));
+// console.log(typeof new String('jonas').slice(1));
+
 // //Maps iteration -------------------------
 // const question = new Map([
 //   ['question', 'What is the best programming language in the world?'],
@@ -667,4 +702,50 @@ const game = {
 // for (const [team, odds] of Object.entries(game.odds)) {
 //   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
 //   console.log(`Odd of ${teamStr}: ${odds}`);
+// }
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// //1.
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
+
+// //2.
+// gameEvents.delete(64);
+// console.log(gameEvents);
+// console.log(gameEvents.has(64));
+
+// // 3. "An event happened, on average, every 9 minutes"
+// const averageEvent = 90 / gameEvents.size;
+// console.log(`An event happend, on average every ${averageEvent} minutes`);
+
+// //4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+// //[FIRST HALF] 17: ⚽️ GOAL
+
+// for (const [key, value] of gameEvents) {
+//   const gameTime = key <= 45 ? '[First halv]' : '[Second halv]';
+//   console.log(`${gameTime} ${key}: ${value}`);
 // }
